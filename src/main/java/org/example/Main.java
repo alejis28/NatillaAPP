6cb43ae70be4f8c0ee7b3de0249f9105f81db732
you@example.com <you@example.com>
2023-12-04 15:36:44 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //entradas del problema
+
+        String nombreUsuario="Alejandra Arango";
+        short edadUsuario=24;
+        String telefonoUsuario="3043332566";
+        String documentoIdentida="1001362854";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="calle 68a# 32-60";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //proceso
+        //1. asignar una valor especifico a una variable
+        // desde el teclado (recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre");
+        leerDato.nextLine();
+
+    }
+}
